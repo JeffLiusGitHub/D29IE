92d9bdbc37d9b5b18d9b3734f0525ddd72d4c8aa
jeffliu <zliu0054@student.monash.edu>
2020-04-27 02:47:59 +1000

    quiz index, quiz 2, Map/crash_choropleth.html combine but python cannot play with javascript

---
+++ ie_sport_injury ver 1.3/Controllers/Quiz2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ie_sport_injury_ver_1._3.Controllers
+{
+    public class Quiz2Controller : Controller
+    {
+        // GET: Quiz2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
