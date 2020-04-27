09d8bdf93cafe886cf54d0975bb8aa890c474543
jeffliu <zliu0054@student.monash.edu>
2020-04-28 00:50:32 +1000

    Add password page

---
+++ ie_sport_injury ver 1.3/Controllers/PasswordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ie_sport_injury_ver_1._3.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: Password
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
